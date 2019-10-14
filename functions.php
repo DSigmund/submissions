9ce84f14e644b7b037b9b1939de4de1614878577
Dominik Sigmund <dominik.sigmund@br.de>
2019-10-14 19:12:11 +0200

    Try 4

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function unescape($input) {
   $output = html_entity_decode($input);
-  $output = str_replace("\\", "",output);
+  $output = str_replace("\\", "", $output);
   return $output;
 }
 ?>
