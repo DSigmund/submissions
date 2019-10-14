474903c95025d114cc7f50616b3e91d479360168
Dominik Sigmund <dominik.sigmund@br.de>
2019-10-14 18:58:30 +0200

    Added Test File

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+$id = $_GET["id"];
+// Create CSV for Form by ID
+
+// 0. require config and connect to database
+require_once('config.php');
+$mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$mysql->query("SET NAMES utf8"); 
+if ($mysql->connect_error) {
+  die("Connection failed: " . $mysql->connect_error);
+} 
+
+$sql = "SELECT data_id, name, value FROM Td6PNmU6_cf7_vdata_entry WHERE cf7_id=".$id." AND name NOT LIKE \"\\_%\" ORDER BY data_id ASC, name ASC";
+
+$result = $mysql->query($sql);
+
+$entries = array();
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    // 1. Fill Named array with fields
+    $entries[$row["data_id"]]["data_id"] = $row["data_id"];
+    $entries[$row["data_id"]][$row["name"]] = htmlspecialchars(trim(preg_replace("~[\r\n]~", " ",$row["value"])));
+  }
+} else {
+  echo "0 results";
+}
+$mysql->close();
+echo "<pre>";
+print_r($entries);
+echo "</pre>";
+
+die();
+?>
